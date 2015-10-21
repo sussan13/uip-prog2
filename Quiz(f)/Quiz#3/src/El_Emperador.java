c1ab02a6c047d0650d35e98474e5ec3706a719ed
sussan13 <sussanmichele@outlook.com>
2015-10-21 16:07:21 -0500

    Quiz#3

---
+++ Quiz(f)/Quiz#3/src/El_Emperador.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+     public class El_Emperador {
+	
+	   private static Scanner keyboard;
+	   float  t;
+	   static float ope, des;
+	   static float mon;
+			
+	   public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\n\tEl Emperador");
+		
+		keyboard = new Scanner (System.in);
+		System.out.print("\nIngrese el monto de la compra: ");
+		float mon = keyboard.nextFloat();
+		System.out.println("\nEl Monto de la compra es: " + mon);
+	    
+	     if (mon>=500)	  {
+	    	 ope=(float)(mon*0.30);
+	    	 des=(float)(mon-ope);
+	    	 System.out.println("\nEl descuento de la compra del 30% es: " +ope);
+	    	 System.out.println("\nEl total de la compra es : " +des); 	 
+	     }
+	     
+	     else if (mon<500 && mon>=200){
+	    	 ope=(float)(mon*0.20);
+	    	 des=(float)(mon-ope);
+	    	 System.out.println("\nEl descuento de la compra del 20% es: " +ope);
+	    	 System.out.println("\nEl total de la compra es : " +des);           
+	    }
+	     else if (mon<200 && mon>=100){
+	    	 ope=(float)(mon*0.10);
+	    	 des=(float)(mon-ope);
+	    	 System.out.println("\nEl descuento de la compra del 10% es: " +ope);
+	    	 System.out.println("\nEl total de la compra es : " +des);
+	     }
+     }
+     }
+       
